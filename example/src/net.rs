db4238008f0bb97f0b82550ef949c9a32ead59d2
yufeng <321353225@qq.com>
2023-03-02 20:44:19 +0800

    add a http sample

--- example/src/net.rs
+++ example/src/net.rs
@@ -1,11 +1,10 @@
-
 use core::ptr::NonNull;
 
 use alloc::format;
-use alloc::{vec, string::String};
-use alloc::vec::Vec;
-use lose_net_stack::{LoseStack, IPv4, MacAddress, results::Packet, TcpFlags};
-use opensbi_rt::{print, println};
+use alloc::{string::String, vec};
+use lose_net_stack::packets::tcp::TCPPacket;
+use lose_net_stack::{results::Packet, IPv4, LoseStack, MacAddress, TcpFlags};
+use opensbi_rt::{print, println, sbi};
 // use virtio_drivers::{VirtIONet, VirtIOHeader, MmioTransport};
 use virtio_drivers::device::net::VirtIONet;
 use virtio_drivers::transport::mmio::{MmioTransport, VirtIOHeader};
@@ -14,10 +13,15 @@ use crate::virtio_impls::HalImpl;
 
 pub fn init() {
     let mut net = VirtIONet::<HalImpl, MmioTransport>::new(unsafe {
-        MmioTransport::new(NonNull::new(0x1000_8000 as *mut VirtIOHeader).unwrap()).expect("failed to create net driver")
-    }).expect("failed to create net driver");
+        MmioTransport::new(NonNull::new(0x1000_8000 as *mut VirtIOHeader).unwrap())
+            .expect("failed to create net driver")
+    })
+    .expect("failed to create net driver");
 
-    let lose_stack = LoseStack::new(IPv4::new(10, 0, 2, 15), MacAddress::new([0x52, 0x54, 0x00, 0x12, 0x34, 0x56]));
+    let lose_stack = LoseStack::new(
+        IPv4::new(10, 0, 2, 15),
+        MacAddress::new([0x52, 0x54, 0x00, 0x12, 0x34, 0x56]),
+    );
 
     loop {
         info!("waiting for data");
@@ -32,73 +36,77 @@ pub fn init() {
 
         match packet {
             Packet::ARP(arp_packet) => {
-                let reply_packet = arp_packet.reply_packet(lose_stack.ip, lose_stack.mac).expect("can't build reply");
-                net.send(&reply_packet.build_data()).expect("can't send net data");
-            },
+                let reply_packet = arp_packet
+                    .reply_packet(lose_stack.ip, lose_stack.mac)
+                    .expect("can't build reply");
+                net.send(&reply_packet.build_data())
+                    .expect("can't send net data");
+            }
             Packet::UDP(udp_packet) => {
-                info!("{}:{}(MAC:{}) -> {}:{}(MAC:{})  len:{}", udp_packet.source_ip, udp_packet.source_port, udp_packet.source_mac, 
-                    udp_packet.dest_ip, udp_packet.dest_port, udp_packet.dest_mac, udp_packet.data_len);
-                info!("data: {}", String::from_utf8_lossy(udp_packet.data.as_ref()));
+                info!(
+                    "{}:{}(MAC:{}) -> {}:{}(MAC:{})  len:{}",
+                    udp_packet.source_ip,
+                    udp_packet.source_port,
+                    udp_packet.source_mac,
+                    udp_packet.dest_ip,
+                    udp_packet.dest_port,
+                    udp_packet.dest_mac,
+                    udp_packet.data_len
+                );
+                info!(
+                    "data: {}",
+                    String::from_utf8_lossy(udp_packet.data.as_ref())
+                );
                 hexdump(udp_packet.data.as_ref());
 
                 if String::from_utf8_lossy(udp_packet.data.as_ref()) == "this is a ping!" {
                     let data = r"reply".as_bytes();
                     let udp_reply_packet = udp_packet.reply(data);
-                    net.send(&udp_reply_packet.build_data()).expect("can't send using net dev");
+                    net.send(&udp_reply_packet.build_data())
+                        .expect("can't send using net dev");
                     break;
                 }
             }
             Packet::TCP(tcp_packet) => {
                 if tcp_packet.flags == TcpFlags::S {
+                    // receive a tcp connect packet
                     let mut reply_packet = tcp_packet.ack();
                     reply_packet.flags = TcpFlags::S | TcpFlags::A;
                     let reply_data = &reply_packet.build_data();
                     net.send(&reply_data).expect("can't send to net");
                 } else if tcp_packet.flags.contains(TcpFlags::F) {
-                    let mut reply_packet = tcp_packet.ack();
-                    net.send(&reply_packet.build_data()).expect("can't send to net");
+                    // tcp disconnected
+                    let reply_packet = tcp_packet.ack();
+                    net.send(&reply_packet.build_data())
+                        .expect("can't send to net");
 
                     let mut end_packet = reply_packet.ack();
                     end_packet.flags |= TcpFlags::F;
-                    net.send(&end_packet.build_data()).expect("can't send to net");
+                    net.send(&end_packet.build_data())
+                        .expect("can't send to net");
                 } else {
-                    info!("{}:{}(MAC:{}) -> {}:{}(MAC:{})  len:{}", tcp_packet.source_ip, tcp_packet.source_port, tcp_packet.source_mac, 
-                    tcp_packet.dest_ip, tcp_packet.dest_port, tcp_packet.dest_mac, tcp_packet.data_len);
-                    info!("data: {}", String::from_utf8_lossy(tcp_packet.data.as_ref()));
+                    info!(
+                        "{}:{}(MAC:{}) -> {}:{}(MAC:{})  len:{}",
+                        tcp_packet.source_ip,
+                        tcp_packet.source_port,
+                        tcp_packet.source_mac,
+                        tcp_packet.dest_ip,
+                        tcp_packet.dest_port,
+                        tcp_packet.dest_mac,
+                        tcp_packet.data_len
+                    );
+                    info!(
+                        "data: {}",
+                        String::from_utf8_lossy(tcp_packet.data.as_ref())
+                    );
 
                     hexdump(tcp_packet.data.as_ref());
                     if tcp_packet.flags.contains(TcpFlags::A) && tcp_packet.data_len == 0 {
                         continue;
                     }
-                    const CONTENT: &str = r#"<html>
-                        <head>
-                        <title>Hello, This is lose-net-stack</title>
-                        </head>
-                        <body>
-                        <center>
-                            <h1>Lose-Net-Stack, website: <a href='http://github.com/yfblock/lose-net-stack'>http://github.com/yfblock/lose-net-stack</a></h1>
-                        </center>
-                        <hr>
-                        </body>
-                        </html>
-                    "#;
-                    let header = format!("\
-HTTP/1.1 200 OK\r\n\
-Content-Type: text/html\r\n\
-Content-Length: {}\r\n\
-Connecion: close\r\n\
-\r\n\
-{}", CONTENT.len(), CONTENT);
-                    // let reply_data = r"th".as_bytes();
-                    // if String::from_utf8_lossy(tcp_packet.data.as_ref()) == "this is a ping!" {
-                    //     let temp_data: Vec<u8> = Vec::new();
-                    //     let mut reply_packet = tcp_packet.reply(reply_data);
-                    //     reply_packet.flags = TcpFlags::A;
-                    //     let reply_data = &reply_packet.build_data();
-                    //     net.send(&reply_data).expect("can't send to net");
-                    // }
-                    let mut reply_packet = tcp_packet.reply(header.as_bytes());
-                    net.send(&reply_packet.build_data()).expect("can't send to");
+
+                    // handle tcp data
+                    receive_tcp(&mut net, &tcp_packet)
                 }
             }
             _ => {}
@@ -134,17 +142,48 @@ pub fn hexdump(data: &[u8]) {
                 print!("{:02} ", "");
             }
         }
-        
+
         println!("");
     }
     println!("{:-^1$}", " hexdump end ", PRELAND_WIDTH);
 }
 
-/* UDP SEND HEXDUMP
------------------------------- hexdump -------------------------------
-ff ff ff ff ff ff 52 54 00 12 34 56 08 00 45 00       ......RT..4V..E.
-28 00 00 00 00 00 20 11 61 ff 0a 00 02 0f 0a 00       (..... .a.......
-02 02 38 18 39 18 14 00 5e ff 48 65 6c 6c 6f 20       ..8.9...^.Hello 
-57 6f 72 6c 64 21                                     World!                              
----------------------------- hexdump end -----------------------------
-*/
+// handle packet when receive a tcp packet
+pub fn receive_tcp(net: &mut VirtIONet<HalImpl, MmioTransport>, tcp_packet: &TCPPacket) {
+    const CONTENT: &str = include_str!("../index.html");
+    let header = format!(
+        "\
+HTTP/1.1 200 OK\r\n\
+Content-Type: text/html\r\n\
+Content-Length: {}\r\n\
+Connecion: close\r\n\
+\r\n\
+{}",
+        CONTENT.len(),
+        CONTENT
+    );
+
+    // is it a get request?
+    if tcp_packet.data_len > 10 && tcp_packet.data[..4] == [0x47,0x45,0x54, 0x20] {
+        let mut index = 0;
+        for i in 4..tcp_packet.data_len {
+            if tcp_packet.data[i] == 0x20 {
+                index = i;
+                break;
+            }
+        }
+
+        let url = String::from_utf8_lossy(&tcp_packet.data[4..index]);
+        info!("request for {}", url);
+        if url == "/close" {
+            let reply_packet = tcp_packet.ack();
+            net.send(&reply_packet.build_data()).expect("can't send reply packet");
+            sbi::legacy::shutdown();
+        }
+        let reply_packet = tcp_packet.reply(header.as_bytes());
+        net.send(&reply_packet.build_data()).expect("can't send to");
+    } else {
+        let reply_packet = tcp_packet.ack();
+        net.send(&reply_packet.build_data()).expect("can't send reply packet");
+    }
+}
--- src/utils.rs
+++ src/utils.rs
@@ -52,16 +52,16 @@ pub fn check_sum(addr:*mut u8, len:u32, sum: u32) -> u16 {
     let mut sum:u32 = sum;
     let mut nleft = len;
     let mut w = addr as *const u16;
-
-    if sum > 0xffff {
-        sum = (sum & 0xFFFF) + (sum >> 16);
-        sum = sum + (sum >> 16);
-    }
     
      while nleft > 1 {
         sum += unsafe{ *w as u32 };
         w = (w as usize + 2) as *mut u16;
         nleft -= 2;
+
+        if sum > 0xffff {
+            sum = (sum & 0xFFFF) + (sum >> 16);
+            sum = sum + (sum >> 16);
+        }
      }
 
      if nleft == 1 {
